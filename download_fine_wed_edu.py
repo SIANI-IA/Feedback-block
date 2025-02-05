424311237d06eeb571fd253cab77bcf9233807a1
miguel-kjh <miguelangel.kjh@gmail.com>
2025-02-05 16:18:59 +0000

    update

--- download_fine_wed_edu.py
+++ download_fine_wed_edu.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
-ds = load_dataset("HuggingFaceTB/smollm-corpus", "fineweb-edu-dedup")
+ds = load_dataset("HuggingFaceTB/smollm-corpus", "fineweb-edu-dedup", split="train", num_proc=16)
 
 print(ds[0])
