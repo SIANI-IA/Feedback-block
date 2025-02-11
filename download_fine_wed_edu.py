86325c94ce12a72e7cfa96d065109d8809d48bab
miguel-kjh <miguelangel-kjh@gmail.com>
2025-02-11 15:05:31 +0000

    update

--- download_fine_wed_edu.py
+++ download_fine_wed_edu.py
@@ -1,5 +1,21 @@
 from datasets import load_dataset
+import os
 
-ds = load_dataset("HuggingFaceTB/smollm-corpus", "fineweb-edu-dedup", split="train", num_proc=16)
+# Cargar el dataset
+ds = load_dataset("HuggingFaceTB/smollm-corpus", "cosmopedia-v2", split="train", num_proc=16)
 
+# Imprimir el primer elemento del dataset
 print(ds[0])
+
+# Crear el directorio donde se guardará el dataset
+ds_dir = "data/pretrain/cosmopedia-v2"
+os.makedirs(ds_dir, exist_ok=True)
+
+# Guardar el dataset en formato JSON o Parquet
+ds.save_to_disk(ds_dir)
+
+
+
+
+
+
